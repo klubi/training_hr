ff3d3e490e2709c09d40cd95828bf58087a4f360
Tomasz Jeż <tomasz.jez@jamfsoftware.com>
2018-11-05 15:50:20 +0100

    correct stuff

--- src/main/java/pl/klubinski/microservices/model/Document.java
+++ src/main/java/pl/klubinski/microservices/model/Document.java
@@ -1,26 +1,27 @@
 package pl.klubinski.microservices.model;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 @Data
 @Setter
 @Getter
 @Entity
-@Table(name = "department")
-public class Department {
+@Table(name = "documents")
+public class Document {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
+  private Long documentId;
 
   @Column
-  private String name;
+  private Long managerId;
 }
--- src/main/java/pl/klubinski/microservices/repository/DocumentRepository.java
+++ src/main/java/pl/klubinski/microservices/repository/DocumentRepository.java
@@ -1,8 +1,9 @@
 package pl.klubinski.microservices.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import pl.klubinski.microservices.model.Document;
 import pl.klubinski.microservices.model.Employee;
 
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+public interface DocumentRepository extends JpaRepository<Document, Long> {
 
 }
